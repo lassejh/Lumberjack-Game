d19b6b03bbc385cc0e7fbd5e11c8e2d901cb5526
Lasse Hansen <lasse.j.hansen@gmail.com>
2018-11-24 14:30:21 +0100

    ObjectPooler added

---
+++ Assets/AddForcePoolTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForcePoolTest : MonoBehaviour , IPooledObject {
+
+    public float upForce = 20f;
+    public float sideForce = .5f;
+
+	public void OnObjectSpawn () {
+
+        float xForce = Random.Range(-sideForce, sideForce);
+        float yForce = Random.Range(upForce / 2f, upForce);
+        float zForce = Random.Range(-sideForce, sideForce);
+
+        Vector3 force = new Vector3(xForce, yForce, zForce);
+
+        GetComponent<Rigidbody>().velocity = force;
+
+    }
+
+    
+	
+
+	void Update () {
+		
+	}
+}
---
+++ Assets/WoodSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodSpawner : MonoBehaviour {
+
+    ObjectPooler objectPooler;
+
+	void Start ()
+    {
+        objectPooler = ObjectPooler.Instance;
+	}
+	
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        objectPooler.SpawnFromPool("Plank", new Vector3(Random.Range (-10f,10f),Random.Range(0.05f,2f),Random.Range(-10f,10f)), Quaternion.Euler(new Vector3(Random.Range(0f,360f),Random.Range(0f,360f), Random.Range(0f, 360f))));
+    }
+}
