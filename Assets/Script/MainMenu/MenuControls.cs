595c2f6901c174334c85b197eaf3c1cf8564f66d
Snifle7 <42936539+Snifle7@users.noreply.github.com>
2019-01-13 20:01:52 +0100

    Main Menu
    In Progress

---
+++ Assets/Script/MainMenu/MenuControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControls : MonoBehaviour
+{
+
+    public GameObject menuSlider;
+    public GameObject anyWhereButton;
+    public GameObject anyWhereText;
+    
+
+    public void SlideMenu()
+
+    {
+        menuSlider.GetComponent<Animation>().Play("MenuSlider");
+        anyWhereButton.SetActive(false);
+        anyWhereText.SetActive(false);
+    }
+
+}
