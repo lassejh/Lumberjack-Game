c6a148308c47acde7b8c85b49f272198dd1fd99b
Snifle7 <42936539+Snifle7@users.noreply.github.com>
2019-01-14 12:30:22 +0100

    Main menu + Pause Menu
    WIP

---
+++ Assets/Script/Menu/GameMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+
+    public static bool gamePaused = false;
+    public GameObject gameMenu;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+
+        {
+            if (gamePaused)
+
+            {
+                Resume();
+            }
+
+            else
+
+            {
+                Pause();
+            }
+
+        }
+
+    }
+
+    public void Resume()
+
+    {
+        gameMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause()
+
+    {
+        gameMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void LoadMenu()
+
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+
+    {
+        Debug.Log("IT WORKED!");
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/Menu/MenuControls.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour {
+
+    public GameObject menuSlider;
+    public GameObject anyWhereButton;
+    public GameObject anyWhereText;
+    public AudioSource menuMusic;
+    public GameObject loadingScreen;
+    
+
+    public void SlideMenu()
+
+    {
+        menuSlider.GetComponent<Animation>().Play("MenuSlider");
+        anyWhereButton.SetActive(false);
+        anyWhereText.SetActive(false);
+    }
+
+    public void PlayGame()
+
+    {
+
+        menuMusic.Stop();
+        loadingScreen.SetActive(true);
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+
+    {
+        Application.Quit();
+    }
+
+}
